64c75284a8dc07bb31cfd1bd8ba3ea856122167e
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2022-09-03 21:57:13 -0500

    python scripts added

---
+++ README.md
@@ -0,0 +1,5 @@
+This repository includes codes to study lower-precision arithmetic for
+lossy compression technique using principal component analysis (PCA).
+
+
+
--- comp.py
+++ comp.py
@@ -7,17 +7,24 @@ import sys
 images = np.load('data/sampleimages.npz')['images']
 d = np.array([images[0]])
 data = np.reshape(d, (d.shape[0], d.shape[1]*d.shape[2]))
-print(f'd.shape={d.shape}')
-print(f'data.shape={data.shape}')
-print(f'data.dtype={data.dtype}')
+#print(f'd.shape={d.shape}')
+#print(f'data.shape={data.shape}')
+#print(f'data.dtype={data.dtype}')
 
 S = 3000
 
-reduced_encoding_matrix = np.load('data/enc%d.npz'%S)['mat']
-print(f'reduced_encoding_matrix.shape={reduced_encoding_matrix.shape}')
-inv_encoding_matrix = np.load('data/inv%d.npz'%S)['mat']
-print(f'inv_encoding_matrix.shape={inv_encoding_matrix.shape}')
+if len(sys.argv) > 1:
+    S = int(sys.argv[1])
 
+print(f'S={S}')
+
+
+reduced_encoding_matrix = np.load('data/enc%d.npz'%S)['mat']
+#print(f'reduced_encoding_matrix.shape={reduced_encoding_matrix.shape}')
+inv_encoding_matrix = np.load('data/inv%d.npz'%S)['mat']
+#print(f'inv_encoding_matrix.shape={inv_encoding_matrix.shape}')
+
+#
 #
 #
 
@@ -29,18 +36,18 @@ for i in range(0,d.shape[1]*d.shape[2],step):
     m2 = inv_encoding_matrix[i:(i+step),:S]
     #print(f'm2: {np.min(m2)} {np.max(m2)}')
     tmp = np.matmul(m1, m2)
-    np.savez('output/S%d-mat1-%d.npz' % (S, idx), mat=m1)
-    np.savez('output/S%d-mat2-%d.npz' % (S, idx), mat=m2)
-    np.savez('output/S%d-orac-%d.npz' % (S, idx), mat=tmp)
+    #np.savez('output/S%d-mat1-%d.npz' % (S, idx), mat=m1)
+    #np.savez('output/S%d-mat2-%d.npz' % (S, idx), mat=m2)
+    #np.savez('output/S%d-orac-%d.npz' % (S, idx), mat=tmp)
     idx += 1
     #print(f'i={i} {tmp.shape}')
     weighting_matrix = np.add(weighting_matrix, tmp)
 
 weighting_matrix_ref = np.matmul(data, inv_encoding_matrix[:, :S])
-print(f'weighting_matrix_ref.shape={weighting_matrix_ref.shape}')
+#print(f'weighting_matrix_ref.shape={weighting_matrix_ref.shape}')
 
 absdiff = np.absolute(weighting_matrix_ref-weighting_matrix)
-print(f'diff: min={np.min(absdiff)}  max={np.max(absdiff)}')
+#print(f'diff: min={np.min(absdiff)}  max={np.max(absdiff)}')
 
 
 
@@ -50,20 +57,25 @@ print(f'diff: min={np.min(absdiff)}  max={np.max(absdiff)}')
 data_approx = np.matmul(weighting_matrix, reduced_encoding_matrix)
 data_approx = np.clip(data_approx, 0, np.inf)
 mse = np.sum((data - data_approx)**2) / (d.shape[1]*d.shape[2])
-print(f'mse={mse}')
+print(f'mse {mse}')
 
-data_approx_img = np.reshape(data_approx, (1, d.shape[1], d.shape[2]))
-imsave('data_approx%d.png'%S, data_approx_img[0].astype(np.uint16))
+#data_approx_img = np.reshape(data_approx, (1, d.shape[1], d.shape[2]))
+#imsave('data_approx%d.png'%S, data_approx_img[0].astype(np.uint16))
 
-datalow = data
+datalow = data.astype(np.float16)
 invlow = inv_encoding_matrix[:, :S]
-weighting_matrix_low = np.matmul(datalow, invlow, dtype=np.float32)
+weighting_matrix_low = np.matmul(datalow, invlow, dtype=np.float16)
+
 data_approx_low = np.matmul(weighting_matrix_low, reduced_encoding_matrix, dtype=np.float64)
 data_approx_low = np.clip(data_approx_low, 0, np.inf)
 mselow = np.sum((data - data_approx_low)**2) / (d.shape[1]*d.shape[2])
-print(f'mselow={mselow}')
+print(f'mselow {mselow}')
 
+sys.exit(0)
 
+#
+#
+#
 datat = data.transpose().astype(np.float16)
 ss = []
 for i in range(0,S):
@@ -76,5 +88,10 @@ data_approx_mix = np.matmul(weighting_matrix_mix, reduced_encoding_matrix, dtype
 data_approx_mix = np.clip(data_approx_mix, 0, np.inf)
 msemix = np.sum((data - data_approx_mix)**2) / (d.shape[1]*d.shape[2])
 print(f'msemix={msemix}')
+
+
+
+
+
 
 sys.exit(0)
